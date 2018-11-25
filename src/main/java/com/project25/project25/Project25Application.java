8a7ff97bae68c4665b7b14c4e1242e3f969c3a77
slava <slava_eclipse@mail.ru>
2018-11-25 14:40:13 +0600

    init

---
+++ src/main/java/com/project25/project25/Project25Application.java
@@ -0,0 +1,12 @@
+package com.project25.project25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project25Application.class, args);
+	}
+}
